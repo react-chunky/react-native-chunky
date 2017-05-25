d84ef78c680e15edecce4152541726c8c2fba4a5
I. Dan Calinescu <idancalinescu@gmail.com>
2017-05-25 15:49:32 +0300

    fixed packager starter

--- bin/packager.sh
+++ bin/packager.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 THIS_DIR=$(dirname "$0")
-node "$THIS_DIR/../node_modules/react-native/local-cli/cli.js" start "$@"
+node "$THIS_DIR/../../react-native/local-cli/cli.js" start "$@"
