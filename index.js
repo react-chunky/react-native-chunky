3c06c4810065069cb0edee3347e778463a0de7ff
I. Dan Calinescu <idancalinescu@gmail.com>
2017-05-26 23:20:13 +0300

    added remotedev support

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { Core } from 'react-chunky'
 import { AppRegistry, AsyncStorage, Text } from 'react-native'
 
-import Reactotron from 'reactotron-react-native'
-import { reactotronRedux } from 'reactotron-redux'
-
 import * as Styles from './src/styles'
 import * as Errors from './src/errors'
 import Screen from './src/core/Screen'
@@ -14,9 +11,6 @@ import App from './src/core/App'
 global.localStorage = AsyncStorage
 
 export function renderApp(props) {
-  // Connect to Reactotron
-  Reactotron.configure({ name: 'Chunky' }).use(reactotronRedux()).connect()
-  Reactotron.log('Starting Chunky')
   const main = () => (<Core.AppContainer {...props}>
     <App {...props}/>
   </Core.AppContainer>)
